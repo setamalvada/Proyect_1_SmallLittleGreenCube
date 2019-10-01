e7f9548164e62062e7958c7d12d933e0ceb8390d
Clara Rodriguez <clara92r@gmail.com>
2019-10-01 21:43:11 +0200

    game logic updated, still no jump in platform

--- game.js
+++ game.js
@@ -7,14 +7,13 @@ class Game {
 
         //this.bg = new Background(ctx)
         //this.car = new Car(ctx)  
-        this.intervalId = null;
+        this.intervalId = null
         //this.intervalIdTime = null;
-        this.obstacles = [
-         new Obstacle(ctx,700,300,300,30),
+        this.obstacles = new Obstacle(ctx,700,320,300,100)
          //new Obstacle(ctx,1000,200,200,30),
          //new Obstacle(ctx,1250,150,200,30),
          
-        ]
+        
     
         //this.tick = 0
 
@@ -30,9 +29,14 @@ class Game {
       
       //this._clearObstacles()
       //this._checkCollisions()
-      //this.crashWithUpperParts()
-      this.updateGameArea()
-      console.log(this.player.y)
+      this.player.crashWithTopObstacle(this.obstacles)
+      this.player.obstacleDetection(this.obstacles)
+      this.player.checkIsFloor()
+      this.player.runningOnObstacle(this.obstacles)
+      //this.checkJumpInObstacle()
+      //this.updateGameArea()
+     console.log(this.player.vy)
+      //console.log(this.player.y)
       //console.log(this.obstacles[0].y)
     }, 1000 / 60)
   }
@@ -41,7 +45,7 @@ class Game {
   draw() {
     this.bg.draw()
     this.player.draw()
-    this.obstacles.forEach(o => o.draw())
+    this.obstacles.draw()
 /*
     this.tick++
 
@@ -53,21 +57,34 @@ class Game {
   _move() {
     this.bg.move()
     this.player.move()
-    this.obstacles.forEach(o => o.move())
+    this.obstacles.move()
   }
 
   
-  updateGameArea(){
-    if(this.player.crashWithUpperParts(this.obstacles)){
-      for (let i = 0; i < this.obstacles.length; i++) { 
-      this.player.y=this.obstacles[i].y-this.obstacles[i].h
-      }
-    }
-    else{
-      this.player.y = this.player.y0
-    }
+  /*updateGameArea(){
+  if(this.player.crashWithUpperParts(this.obstacles)){
+    this.player.y = this.obstacles.y-(this.obstacles.h+this.obstacles.h/2)
+    
   }
+
+  else{
+    this.vy = -this.vy
+  }*/
+      //for (let i = 0; i < this.obstacles.length; i++) { 
+      
+      
+      //}
+    
+   /* else if (this.player.checkPos(this.obstacles)){
+      this.player.y0 = 350
+    }
+  }*/
   
+  /*checkJumpInObstacle(){
+    if(this.player.crashWithUpperParts(this.obstacles)){
+      this.player._jump()
+    }
+  }*/
   
   _checkCollisions(){
     const col = this.obstacles.some(o => {
--- player.js
+++ player.js
@@ -1,112 +1,113 @@
-const SPACE_KEY = 32
+const SPACE_KEY = 32;
 
 class Player {
-    constructor(ctx){
-       
-        this.ctx=ctx
-        this.x=100
-        this.y0=350
-        this.y=this.y0
-        this.w=50
-        this.h=50
-        this.vy= 3
-        this.ay=0.8        
-        this.h0=0
-        this.y0=350
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.x = 100;
+    this.y0 = 350;
+    this.y = this.y0;
+    this.w = 50;
+    this.h = 50;
+    this.vy = 3;
+    this.ay = 0.8;
+    this.h0 = 0;
+    this.y0 = 350;
+    this.jumping = true;
 
-        this._setListeners()
-        //this.crashWithUpperParts(obs)
-        //this.obstacle= [new Obstacle(ctx,700,700,300,30)]
+    this._setListeners();
+    //this.crashWithUpperParts(obs)
+    //this.obstacle= [new Obstacle(ctx,700,700,300,30)]
+  }
 
+  draw() {
+    this.ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+
+  move() {
+    if (this.y < this.y0) {
+      this.vy += this.ay;
+      this.y += this.vy;
+    } else {
+      this.vy = 0;
+      this.y = this.y0;
     }
+  }
 
-    draw() {
-      this.ctx.fillRect(
-        this.x,
-        this.y,
-        this.w,
-        this.h
-      );
-    }
-
-    move(){
-       
-        if (this.y < this.y0) {
-          this.vy += this.ay;
-          this.y += this.vy;
-        } else {
-          this.vy = 0;
-          this.y = this.y0;
-         
-          
-        }
-    
-       
-      
-    }
-
-    _setListeners() {
-        document.onkeydown = (e) => {
-          if (e.keyCode === SPACE_KEY) {
-            this._jump()
-          }
-        }
-    }
-
-    _jump() {
-        if (!this._isJumping()) {
-         
-       
-          this.y -= 20;
-          this.vy -= 15;
-        }
+  _setListeners() {
+    document.onkeydown = e => {
+      if (e.keyCode === SPACE_KEY) {
+        this._jump();
       }
+    };
+  }
 
-      _isJumping() {
-        return this.y < this.y0
-      }
-/*
-      */
-    
-    
-      crashWithUpperParts = (obs) => {
-      
-    
-        
-        
-        const myleft = this.x;
-        const myright = this.x+(this.w);
-        const mybottom = this.y + (this.h);
-        const mytop= this.y
-        const otherleft = obs.x;
-        const otherright = obs.x +(obs.w);
-        const otherbottom = obs.y + (obs.h);
-        const othertop = obs.y;
+  _jump() {
+    if (!this._isJumping()) {
+      this.jumping = true;
+      this.y -= 20;
+      this.vy -= 15;
+    }
+  }
 
-        /*
-        const myleft = this.x;
-        const myright = this.x+(this.w);
-        const mybottom = this.y + (this.h);
-        const mytop= this.y
-        const otherleft = this.obs[i].x;
-        const otherright = this.obs[i].x +(this.obs[i].w);
-        const otherbottom = this.obs[i].y + (this.obs[i].h);
-        const othertop = this.obs[i].y;
-        
-        */
+  _isJumping() {
+    return this.y < this.y0;
+  }
 
-        
-        let crash = false
+  runningOnObstacle = obsta => {
+    if (this.crashWithUpperParts(obsta) === 1) {
+      this.vy = 0;
+      //const bottomPlayer = this.y +this.h
+      this.y = obsta.y - this.h;
+    }
+  };
+
+  crashWithTopObstacle = obs => {
+    let crash = 0;
+
+    if(this.x +this.h === obs.y){
+      debugger
+    }
+
+    const colX = this.x + this.w > obs.x && this.x < obs.x + obs.w
+    const colY = this.y + this.h > obs.y && this.y < obs.y + obs.h
+
     
-        if (mybottom<othertop){
-          crash = false
-        }
-        else if(myleft>=otherleft){
-          crash = true
-        }
-        console.log(crash)
-        return crash
-       
-      }
-  
+
+    if (this.x + this.w === obs.x && this.y + this.h < obs.y) {
+      crash = 1;
+    } else if (this.x + this.w === obs.x && this.y > obs.y + obs.h) {
+      crash = 2;
+    } else if (
+      this.x + this.w === obs.x &&
+      obs.y <= this.y &&
+      this.y <= obs.y + obs.h
+    ) {
+      crash = 3;
+    } else if (
+      obs.x < this.x + this.w &&
+      this.x + this.w < obs.x + obs.w &&
+      this.y + this.h === obs.y
+    ) {
+      crash = 4;
+    }
+
+    console.log("crash " + crash);
+
+    return crash;
+  };
+
+  obstacleDetection = obst => {
+    if (this.x > obst.x + obst.w) {
+      this.vy = this.vy * -1;
+    }
+  };
+
+  checkIsFloor() {
+    let floor = false;
+    if (this.y >= this.ctx.canvas.height - this.h) {
+      floor = true;
+    }
+    console.log("floor " + floor);
+    return floor;
+  }
 }
